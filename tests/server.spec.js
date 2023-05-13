74a9f02faec87cf0a2d191fd7d017b9ce7c6e2fe
Pipexlul <felipgg@gmail.com>
2023-05-13 13:17:39 -0400

    Make jest files work with ESM

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -1,6 +1,8 @@
-const request = require("supertest");
-const server = require("../index");
+import request from "supertest";
+import server from "../index.js";
 
 describe("Operaciones CRUD de cafes", () => {
-
+  it("GET /cafes", () => {
+    expect(true).toBe(true);
+  });
 });
