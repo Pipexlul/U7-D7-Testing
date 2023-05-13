89cc01f15919f507da240889962633d98c6d45ce
Pipexlul <felipgg@gmail.com>
2023-05-13 14:44:24 -0400

    Implement test case 1 and test case 2

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -1,8 +1,28 @@
 import request from "supertest";
 import server from "../index.js";
 
+import { describe, it, expect } from "@jest/globals";
+
+const app = request(server);
+
 describe("Operaciones CRUD de cafes", () => {
-  it("GET /cafes", () => {
-    expect(true).toBe(true);
+  describe("Chequear ruta GET /cafes", () => {
+    it("Deberia devolver un status code de 200 y un array de cafes con al menos 1 objeto dentro del array", async () => {
+      const response = await app.get("/cafes").send();
+      expect(response.statusCode).toBe(200);
+      expect(Array.isArray(response.body)).toBe(true);
+      expect(response.body.length).toBeGreaterThan(0);
+    });
+  });
+
+  describe("Chequear ruta DELETE /cafes/:id", () => {
+    it("Deberia devolver un status code de 404 si se intenta eliminar un cafe que no existe", async () => {
+      const response = await app
+        .delete("/cafes/999999999")
+        .set("Authorization", "Bearer token")
+        .send();
+
+      expect(response.statusCode).toBe(404);
+    });
   });
 });
